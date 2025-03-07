9d50d96ffe74dea49aa87c79d96bd5fd03b2ffe0
teich <roi.teichman@campus.technion.ac.il>
2025-03-07 09:57:57 +0200

    new file includes the global consts

---
+++ consts.py
@@ -0,0 +1,25 @@
+# Constants
+lower_bound_l = 50
+upper_bound_l = 150
+lower_bound_n = 100
+upper_bound_n = 1000000
+lower_bound_p = 0.001
+upper_bound_p = 0.1
+
+def get_lower_bound_l():
+    return lower_bound_l
+
+def get_upper_bound_l():
+    return upper_bound_l
+
+def get_lower_bound_n():
+    return lower_bound_n
+
+def get_upper_bound_n():
+    return upper_bound_n
+
+def get_lower_bound_p():
+    return lower_bound_p
+
+def get_upper_bound_p():
+    return upper_bound_p
